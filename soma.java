a29fdedccc45d1d20bc6cc90b78a5e4fe86da3f4
John Doe <lucashaleluia@hotmail.com>
2023-08-24 17:40:19 -0300

    add file

---
+++ soma.java
@@ -0,0 +1,9 @@
+
+public class soma{
+    public static void main(String[] args){
+        int a = 12;
+        int b = 40;
+        int soma = a + b;
+        System.out.println("A soma de " + a + " + " + b + " é " + soma);
+    }
+}
